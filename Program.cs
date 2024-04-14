316de167412ede85ec130cd0e46a576654bd2f40
ash <ash@localhost.localdomain>
2024-04-14 19:33:59 +0100

    hello commit

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-// hello zaki 
+// hello zaki lp
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
