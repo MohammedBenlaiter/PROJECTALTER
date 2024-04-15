d8dffc4d93c4ae6ad998d1cac8f00cce3db4f5e7
ash <ash@localhost.localdomain>
2024-04-15 04:39:54 +0100

    test

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -18,7 +18,7 @@ namespace PROJECTALTERAPI.Controllers
         [HttpGet("getAllUser")] // Route at method level
         public async Task<IActionResult> Get()
         {
-            var users = await _db.Users.ToListAsync(); // Asynchronous retrieval
+            var users = await _db.Users.ToListAsync(); //  retrieval
 
             return Ok(users); // Return successful response with users d 
         }
