f123bb953102d8d2710cb65473d7a775544419b0
ash <ash@localhost.localdomain>
2024-05-23 17:49:58 +0100

    fix the database

--- Controllers/ExchangeController.cs
+++ Controllers/ExchangeController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
 using PROJECTALTERAPI.Dtos;
 using PROJECTALTERAPI.Hubs;
 
@@ -11,22 +16,24 @@ namespace PROJECTALTERAPI.Controllers
     {
         private readonly AlterDbContext _context;
         private readonly IHubContext<ChatHub> _hubContext;
+        private IConfiguration _configuration;
 
 
-        public ExchangeController(AlterDbContext context, IHubContext<ChatHub> hubContext)
+        public ExchangeController(AlterDbContext context, IHubContext<ChatHub> hubContext, IConfiguration configuration)
         {
             _context = context;
             _hubContext = hubContext;
+            _configuration = configuration;
         }
 
-        [HttpPost("CreateExchange/{id_rec}/{id_send}/{id_s_rec}/{id_s_send}")]
-        public IActionResult CreateExchange(long id_rec, long id_s_rec, long id_send, long id_s_send)
+        [HttpPost("CreateExchange/{id_rec}/{id_s_rec}")]
+        public IActionResult CreateExchange(long id_rec, long id_s_rec)
         {
+            var user = GetCurrentUser();
             var dto = new ExchangeDto
             {
                 ReciverId = id_rec,
-                SenderId = id_send,
-                SkillSendId = id_s_send,
+                SenderId = user.UserId,
                 SkillReceiveId = id_s_rec,
                 Statues = "sended"
             };
@@ -34,7 +41,6 @@ namespace PROJECTALTERAPI.Controllers
             {
                 ReciverId = dto.ReciverId,
                 SenderId = dto.SenderId,
-                SkillSendId = dto.SkillSendId,
                 SkillReceiveId = dto.SkillReceiveId,
                 Statues = "sended"
             };
@@ -50,6 +56,55 @@ namespace PROJECTALTERAPI.Controllers
             _hubContext.Clients.User(id_rec.ToString()).SendAsync("ReceiveNotification", notification);
 
             return Ok(dto);
+        }
+        [HttpGet("ExchangeNotification/{id}")]
+        public IActionResult ExchangeNotification(long id)
+        {
+            //var user = GetCurrentUser();
+            var exchanges = _context.Exchanges.Where(e => e.ReciverId == id && e.Statues == "sended").ToList();
+            return Ok(exchanges);
+        }
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"] ?? string.Empty));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+             _configuration["Jwt:Audience"],
+              claims,
+               expires: DateTime.Now.AddHours(30),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        private User Auth(LoginDto dto)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == dto.Username);
+            var passwordHasher = new PasswordHasher<User>();
+            if (user != null)
+            {
+                if (passwordHasher.VerifyHashedPassword(user, user.Password, dto.Password) == PasswordVerificationResult.Success)
+                {
+                    return user;
+                }
+            }
+            return null!;
+        }
+        private User GetCurrentUser()
+        {
+            var Identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (Identity != null)
+            {
+                var userClaim = Identity.Claims;
+                return new User
+                {
+                    UserId = Convert.ToInt64(userClaim.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value),
+                    Username = userClaim.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value ?? string.Empty
+                };
+            }
+            return null!; // Add a return statement for the case when Identity is null
         }
     }
 
--- Models/Exchange.cs
+++ Models/Exchange.cs
@@ -11,19 +11,13 @@ public partial class Exchange
 
     public long SenderId { get; set; }
 
-    public long SkillSendId { get; set; }
-
     public long SkillReceiveId { get; set; }
 
     public string Statues { get; set; } = null!;
-
-    public virtual ICollection<Date> Dates { get; set; } = new List<Date>();
 
     public virtual User Reciver { get; set; } = null!;
 
     public virtual User Sender { get; set; } = null!;
 
     public virtual Skill SkillReceive { get; set; } = null!;
-
-    public virtual Skill SkillSend { get; set; } = null!;
 }
