00c07aab1cfc76bab26f25c1ea757e53657967c0
MohammedBenlaiter <benlaitermohammed@outlook.com>
2024-05-29 19:26:44 +0100

    Search users

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -38,6 +38,23 @@ namespace PROJECTALTERAPI.Controllers
 
             return Ok(users); // Return successful response with users d 
         }
+        [HttpGet("searchUser")]
+        public IActionResult SearchUser(SearchDto searchTerm)
+        {
+            var users = _db.Users.Where(u => u.FirstName.Contains(searchTerm.Query) || u.LastName.Contains(searchTerm.Query) || u.Username.Contains(searchTerm.Query)).ToList();
+            if (users.Count == 0)
+            {
+                return NotFound("No users found");
+            }
+            var userDtos = users.Select(u => new UserDto
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Username = u.Username,
+                Password = u.Password
+            }).ToList();
+            return Ok(userDtos);
+        }
         [HttpGet("getUser")]
         public IActionResult GetUser()
         {
--- obj/Debug/net8.0/PROJECTALTERAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/PROJECTALTERAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJECTALTERAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c666a6603b14d1c4aec5e48891330ef826bd42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+104abda9b9722c6504bf5054230b33beed6552ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJECTALTERAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJECTALTERAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
